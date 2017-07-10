b6ea1c64d15e255cfa65202f443b297001c23a51
lixin <lixin.yang@vontechnology.com>
2017-07-10 18:36:59 +1000

    Applied proper looping to saga

--- _app/sagas/someSagas.js
+++ _app/sagas/someSagas.js
@@ -11,15 +11,17 @@ import actionTypes from '../actions/actionTypes';
 import * as actions from '../actions/actions';
 
 function * someSaga() {
+  while(true){
     const doThingAAction = yield take(actionTypes.doThings.doThingA);
     console.log(JSON.stringify(doThingAAction))
-    
+
     const response = yield call(convertToDoThingBPayload, doThingAAction.payload);
     console.log(JSON.stringify(response))
 
     const newAction = actions.doThingB(response);
     console.log(JSON.stringify(newAction))
     yield put(newAction);
+  }
 }
 
 function convertToDoThingBPayload(x) {
